51fcf4d3823cc2c7ef009e16d2f06399421a3c12
John Feminella <jxf@jxf.me>
2017-01-15 14:46:22 -0500

    Use revision:* tasks to write sensible rev-manifest.json

---
+++ tasks/revision.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var rev = require('gulp-rev');
+var del = require('rev-del');
+var gdebug = require('gulp-debug');
+
+var cfg = require('./config');
+
+gulp.task('revision', ['styles' /* [DEBUG] , 'scripts', 'images', 'svg'*/], function() {
+  var temporaryAssetDirectories = cfg.sharedPaths.siteAssetPathGenerator({
+    assetRoots: [cfg.sharedPaths.temporaryAssetRoot()],
+    shouldAddSuffixes: true,
+    assetPropertySource: 'outputs'
+  });
+
+  var outputAssetRoot = cfg.sharedPaths.outputAssets();
+  var temporaryAssetRoot = cfg.sharedPaths.temporaryAssetRoot();
+
+  return gulp.src(temporaryAssetDirectories, {base: temporaryAssetRoot})
+    .pipe(gdebug({title: 'revision[in]:'}))
+    .pipe(rev())
+    .pipe(gulp.dest(outputAssetRoot))
+    .pipe(rev.manifest())
+    .pipe(del({dest: outputAssetRoot}))
+    .pipe(gulp.dest(outputAssetRoot))
+    .pipe(gdebug({title: 'revision[out]:'}));
+});
